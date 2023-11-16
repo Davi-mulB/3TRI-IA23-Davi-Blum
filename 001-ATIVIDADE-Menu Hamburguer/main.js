56a5f254138b1525e96549a0e0af78c6b722c7e0
Davi-mulB <131017124+Davi-mulB@users.noreply.github.com>
2023-11-16 19:55:37 +0000

    Exclui algumas coisas

--- 001-ATIVIDADE-Menu Hamburguer/main.js
+++ 001-ATIVIDADE-Menu Hamburguer/main.js
@@ -5,15 +5,3 @@ btHamburger.addEventListener("click", () => {
   btHamburger.classList.toggle("opened")
   mainMenu.classList.toggle("opened")
 })
-
-// ----------------------------------------------------------------------------------
-
-const btDialog = document.querySelector(".bt-dialog")
-const btModal = document.querySelector(".bt-modal")
-const dialog = document.querySelector("dialog")
-
-btDialog.addEventListener("click", () => dialog.show())
-btModal.addEventListener("click", () => dialog.showModal())
-
-document.querySelectorAll("dialog button").forEach(
-  bt => bt.addEventListener("click", () => dialog.close()))
